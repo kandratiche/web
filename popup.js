6819b00891fa2182640904e4c3429798ccf2dcdd
arlan <elfqqz@gmail.com>
2025-10-12 20:02:43 +0500

    Done

---
+++ popup.js
@@ -0,0 +1,19 @@
+const popupBtns = document.querySelectorAll('.popup-btn');
+const popupForm = document.querySelector('.popup-form');
+const closeBtn = document.querySelector('.close-btn');
+
+popupBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        popupForm.style.display = 'flex';
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    popupForm.style.display = 'none';
+});
+
+popupForm.addEventListener('click', (event) => {
+    if (event.target === popupForm) {
+        popupForm.style.display = 'none';
+    }
+});
